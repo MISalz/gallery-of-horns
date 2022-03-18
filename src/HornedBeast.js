cf1644d84f6bf2b12fcc246072fc485ae216a2cc
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-03-17 20:06:58 -0700

    save work, render button onclick event handlers, bootstrap styling

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,15 +1,46 @@
 
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import './HornedBeast.css';
 
 class HornedBeast extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0,
+      favorite: false
+    }
+  }
+  handleLikes = () => {
+    this.setState({
+      likes: this.state.likes + 1
+    })
+  }
+  favorite = () => {
+    this.setState({
+      favorite: true
+    })
+  }
   render() {
     return (
       <article>
-        <img src={this.props.image_url}
+        <h3>{this.props.title}</h3>
+        <p>{this.state.likes}</p>
+        <p onClick={this.handleLikes}>Favorite</p>
+        <img
+          src={this.props.image_url}
           alt={this.props.title}
           description={this.props.description}
         />
+        <p>{this.props.description}</p>
+
+        <div>{this.state.favorite ? 'I Love this Horn!':''}</div>
+
+        <Button 
+        className="hb-button"
+        onClick={this.favorite}
+        >Favorite
+        </Button>
 
       </article>
     );
--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,19 @@
 
 import React from 'react';
 import HornedBeast from './HornedBeast';
+import './Main.css';
 
 class Main extends React.Component {
+  
   render() {
     let hb = [];
     this.props.data.forEach((beast, index) => {
       hb.push(
-        <HornedBeast
-        imageUrl={beast.image_Url}
-          title={beast.title}
-          description={beast.description}
-          key={index}
+        <HornedBeast imageUrl={beast.image_url} title={beast.title} description={beast.description} key={index}
         />
       )
     })
+    console.log(hb)
     return (
       <main>
         {hb}
